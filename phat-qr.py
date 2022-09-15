18892f9cc2e43c6f290634ef79b26bcdbfafd701
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-09-15 19:21:33 +0200

    further structure

--- phat-qr.py
+++ phat-qr.py
@@ -13,12 +13,10 @@ def get_board_info():
     board_info["ip_address"] = s.getsockname()[0]
     return board_info
 
-
 def get_time():
     e = datetime.datetime.now()
     time_info = "%s/%s//%s:%s:%s" % (e.day, e.month, e.hour, e.minute, e.second)
     return time_info
-
 
 def get_crypto_ticker():
     url="https://api.livecoinwatch.com/coins/single"
@@ -36,19 +34,13 @@ def get_crypto_ticker():
     print(current_price_info)
     return current_price_info
 
-
-def display_info(board_info, time_info, current_price_info):
-    inky_display = auto()
-    inky_display.set_border(inky_display.BLACK)
-    # compose qr code 
-    # data = input("Enter text:")
+def get_compose_image(board_info, time_info, current_price_info):
     img = qrcode.make("http://192.168.2.228/admin")
     print(type(img))
     print(img.size)
     # <class 'qrcode.image.pil.PilImage'>
     # (120, 120)
     img.save('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/wow.png', dpi=(300,300))
-
     #compose qr code on top of background troplet
     im1 = Image.open('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/inky-bcs.png')
     im2 = Image.open('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/wow.png')
@@ -58,26 +50,28 @@ def display_info(board_info, time_info, current_price_info):
     im1.save('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png',dpi=(300,300))
     img = Image.open('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png')
     draw = ImageDraw.Draw(img)
-
     # add text to background-droplet
     font = ImageFont.truetype(r'/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/ARIAL.TTF', 40)
     draw.text((20, 0), time_info)
     draw.text((20, 110), board_info["ip_address"])
     draw.text((120, 40), current_price_info, font = font)
-
     img.save("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")
-
     # rotate image
     Original_Image = Image.open("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")
-    rotated_image1 = Original_Image.transpose(Image.ROTATE_180)
-    img = Image.open(("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")).resize(inky_display.resolution)
-
+    rotated_image_info = Original_Image.transpose(Image.ROTATE_180)
     # Display the data on Inky pHAt
-    inky_display.set_image(rotated_image1)
+    return rotated_image_info
+
+def display_info(rotated_image_info):
+    inky_display = auto()
+    inky_display.set_border(inky_display.BLACK)
+    # compose qr code 
+    inky_display.set_image(rotated_image_info)
     inky_display.show()
 
 if __name__ == "__main__":
     board_info = get_board_info()
     time_info = get_time()
     current_price_info = get_crypto_ticker()
-    display_info(board_info, time_info, current_price_info)
+    rotated_image_info = get_compose_image(board_info, time_info, current_price_info)
+    display_info(rotated_image_info)
