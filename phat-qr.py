5f4c4321bde5397f7e2ba6e9e3232e4d19665370
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-09-15 19:04:15 +0200

    cleaned

--- phat-qr.py
+++ phat-qr.py
@@ -1,20 +1,9 @@
-import glob
-import os
-import time
-from sys import exit
+import glob, qrcode, os, time, datetime, requests, socket, json
+from PIL import Image, ImageDraw, ImageFont
 from font_fredoka_one import FredokaOne
 from inky.auto import auto
 from inky import InkyPHAT
-from PIL import Image, ImageDraw, ImageFont
-import qrcode
-from PIL import Image, ImageDraw, ImageFilter
-import requests, json
 from requests.structures import CaseInsensitiveDict
-import datetime
-import socket
-import os, requests, socket
-
-
 
 def get_board_info():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,7 +20,24 @@ def get_time():
     return time_info
 
 
-def display_info(board_info, time_info):
+def get_crypto_ticker():
+    url="https://api.livecoinwatch.com/coins/single"
+    headers = CaseInsensitiveDict()
+    headers["content-type"]="application/json"
+    headers["x-api-key"] = "52d21667-5475-4a08-9ed2-2756e79470db"
+    data = '{"currency":"EUR","code":"XCH","meta":false}'
+    resp = requests.post(url, headers=headers, data=data)
+    data = json.loads(resp.text)
+    parseData = json.dumps(resp.json())
+    priceObj = json.loads(parseData)
+    B = priceObj["rate"]
+    Z = round(B, 3)
+    current_price_info = str(Z)
+    print(current_price_info)
+    return current_price_info
+
+
+def display_info(board_info, time_info, current_price_info):
     inky_display = auto()
     inky_display.set_border(inky_display.BLACK)
     # compose qr code 
@@ -53,26 +59,11 @@ def display_info(board_info, time_info):
     img = Image.open('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png')
     draw = ImageDraw.Draw(img)
 
-    #api request
-    url="https://api.livecoinwatch.com/coins/single"
-    headers = CaseInsensitiveDict()
-    headers["content-type"]="application/json"
-    headers["x-api-key"] = "52d21667-5475-4a08-9ed2-2756e79470db"
-    data = '{"currency":"EUR","code":"XCH","meta":false}'
-    resp = requests.post(url, headers=headers, data=data)
-    data = json.loads(resp.text)
-    parseData = json.dumps(resp.json())
-    priceObj = json.loads(parseData)
-    B = priceObj["rate"]
-    z = round(B, 3)
-    f = str(z)
-    print(f)
-
     # add text to background-droplet
     font = ImageFont.truetype(r'/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/ARIAL.TTF', 40)
     draw.text((20, 0), time_info)
     draw.text((20, 110), board_info["ip_address"])
-    draw.text((120, 40), f, font = font)
+    draw.text((120, 40), current_price_info, font = font)
 
     img.save("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")
 
@@ -88,4 +79,5 @@ def display_info(board_info, time_info):
 if __name__ == "__main__":
     board_info = get_board_info()
     time_info = get_time()
-    display_info(board_info, time_info)
+    current_price_info = get_crypto_ticker()
+    display_info(board_info, time_info, current_price_info)
