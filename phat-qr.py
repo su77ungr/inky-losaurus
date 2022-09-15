1bd106e44f0c3afd1446caffba5ddb5c1248877d
SU77UNGR <69374354+su77ungr@users.noreply.github.com>
2022-09-15 21:28:29 +0200

    Update phat-qr.py

--- phat-qr.py
+++ phat-qr.py
@@ -34,10 +34,26 @@ def get_crypto_ticker():
     print(current_price_info)
     return current_price_info
 
-def get_compose_image(board_info, time_info, current_price_info):
-    img = qrcode.make("http://192.168.2.228/admin")
+def get_pihole_stats(board_info):
+    api_path = f"http://192.168.2.228/admin/api.php?summary"
+    pihole_stats = dict()
+    query_fields = ["unique_clients", "dns_queries_today", "ads_blocked_today", "ads_percentage_today"]
+    try:
+        api_raw = requests.get(api_path, verify=False, timeout=5).json()
+        pihole_stats["status"] = "Connected"
+        for field in query_fields:
+            pihole_stats[field] = api_raw[field]
+    except:
+        pihole_stats["status"] = "DOWN"
+        for field in query_fields:
+            pihole_stats[field] = "-----"
+    return pihole_stats
+
+def get_compose_image(board_info, time_info, current_price_info, pihole_stats):
+    img = qrcode.make("http://"+ board_info["ip_address"] + "/admin")
     print(type(img))
     print(img.size)
+    print(pihole_stats["ads_blocked_today"])
     # <class 'qrcode.image.pil.PilImage'>
     # (120, 120)
     img.save('/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/wow.png', dpi=(300,300))
@@ -52,14 +68,20 @@ def get_compose_image(board_info, time_info, current_price_info):
     draw = ImageDraw.Draw(img)
     # add text to background-droplet
     font = ImageFont.truetype(r'/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/ARIAL.TTF', 40)
+
     draw.text((20, 0), time_info)
+
+    # mini pihole dashboard
+    draw.text((120, 90), (str("Blocked " + "  >" + str(pihole_stats["dns_queries_today"]))))
+    draw.text((120, 100), (str("Percent" + "   >" + str(pihole_stats["ads_percentage_today"]))))
+    draw.text((120, 110), (str(pihole_stats["status"] + " >" + str(pihole_stats["unique_clients"]))))
     draw.text((20, 110), board_info["ip_address"])
     draw.text((120, 40), current_price_info, font = font)
     img.save("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")
     # rotate image
     Original_Image = Image.open("/home/kube-worker-1/Pimoroni/inky/examples/phat/resources/images/666.png")
     rotated_image_info = Original_Image.transpose(Image.ROTATE_180)
-    # Display the data on Inky pHAt
+    
     return rotated_image_info
 
 def display_info(rotated_image_info):
@@ -72,6 +94,7 @@ def display_info(rotated_image_info):
 if __name__ == "__main__":
     board_info = get_board_info()
     time_info = get_time()
+    pihole_stats = get_pihole_stats(board_info)
     current_price_info = get_crypto_ticker()
-    rotated_image_info = get_compose_image(board_info, time_info, current_price_info)
+    rotated_image_info = get_compose_image(board_info, time_info, current_price_info, pihole_stats)
     display_info(rotated_image_info)
