e972abaae09232c4e6ef7a85b3f441fe76065197
su77ungr <you@example.com>
2022-09-16 20:07:52 +0200

    bumping up README documentation

--- inky-install.sh
+++ inky-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #setup of inky-losaurus
-curl https://get.pimoroni.com/inky | bash &&
-git clone https://github.com/su77ungr/inky-losaurus.git
-cd inky-losaurus 
-sudo pip install -r requirements.txt 
+curl https://get.pimoroni.com/inky | bash
+git clone https://github.com/su77ungr/inky-losaurus.git 
+cd inky-losaurus
+sudo pip install -r requirements.txt
 sudo apt-get install jq -y 
 
 # init first setup to generate config.json
@@ -23,6 +23,6 @@ crontab mycron
 # remove installation files
 rm mycron
 rm path.yml
-
 echo "Setup complete -> edit config.json to change parameters to your liking!"
-echo cat config.json | jq .version 
+echo $(cat config.json | jq .version)
+echo "setup finished - have a great day (～￣▽￣)～"
